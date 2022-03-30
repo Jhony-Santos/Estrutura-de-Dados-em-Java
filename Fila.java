3977a9cc6022eaf2545ff1d1c2315b4407b02966
jntd <jntd@gft.com>
2022-03-30 08:56:27 -0300

    estrutura em andamento

--- Fila.java
+++ Fila.java
@@ -100,22 +100,79 @@ public class Fila {
 
     }
 
-    public static void main(String [] args){
-        Fila fila= new Fila(10);
-        fila.insere(101);
-        fila.insere(102);
-        fila.insere(103);
-        fila.insere(104);
-        fila.mostrarElementos();
-        fila.mostrarPrimeiro();
+
+    public static void main(String [] args) {
+
+        Fila filaA = new Fila(4);
+        filaA.insere(12);
+        filaA.insere(35);
+        filaA.insere(52);
+        filaA.insere(64);
+
+        Fila filaB = new Fila(5);
+        filaB.insere(05);
+        filaB.insere(15);
+        filaB.insere(23);
+        filaB.insere(55);
 
 
+
+        Fila filamerge = null;
+        filamerge=filamerge.merge(filaA, filaB);
+        filamerge.mostrarElementos();
+
+    }
+
+        public Fila merge(Fila filaA, Fila filaB ){
+
+            int tamanhoFilaC = filaA.tamanho + filaB.tamanho;
+            Fila filaC = new Fila(tamanhoFilaC);
+
+
+
+            for(int contador=0; contador < tamanhoFilaC; contador++ )
+            {
+
+                if(filaA.primeiro < filaB.primeiro)
+                {
+                    filaC.insere(filaA.primeiro);
+                    filaA.remove();
+                    //filaC.insere(filaA.remove());
+
+                }
+                else if ( filaA.primeiro >= filaB.primeiro){
+                    filaC.insere(filaB.primeiro);
+                    filaB.remove();
+
+                    //filaC.insere(filaB.remove());
+                }
+                else if(filaA.filaVazia()){
+
+                    for (int i=0;i>filaB.tamanho;i++){
+                        filaC.insere(filaB.primeiro);
+                        filaB.remove();
+                        //filaC.insere(filaB.remove());
+                    }
+                }
+                else if (filaB.filaVazia()){
+                    for(int i=0;i > filaA.tamanho;i++){
+
+                        filaC.insere(filaA.primeiro);
+                        filaA.remove();
+                        //filaC.insere(filaA.remove());
+
+                    }
+
+
+
+                }
+
+            }
+
+            return filaC;
+        }
 
     }
 
 
 
-
-
-
-}
