18de033be7ae943c0eea1d10beb04eee49fbcd3b
jntd <jntd@gft.com>
2022-04-26 10:55:54 -0300

    novos métodos referenciados

--- ListaEncadeada/Main.java
+++ ListaEncadeada/Main.java
@@ -1,4 +1,26 @@
 package ListaEncadeada;
 
 public class Main {
+
+    public static void main(String[] args) {
+        /*Node  lista = new Node();
+        lista.insereOrdenado(10);
+        lista.insereOrdenado(3);
+        lista.insereOrdenado(14);
+        lista.insereOrdenado(11);
+        lista.insereOrdenado(1);
+        lista.insereOrdenado(7);
+        lista.inserePrimeiro(0);
+        lista.insereUltimo(12);
+        lista.imprimir();
+        String espaco=("\n");
+
+
+
+        lista.retira_depois(lista.proximo);
+        lista.imprimir();*/
+
+
+
+    }
 }
