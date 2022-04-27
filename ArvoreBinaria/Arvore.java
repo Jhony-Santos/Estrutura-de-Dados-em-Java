8b26f7ebce7820d245a6764dcc450fe4751b6685
jntd <jntd@gft.com>
2022-04-27 15:08:33 -0300

    métodos inseridos

--- ArvoreBinaria/Arvore.java
+++ ArvoreBinaria/Arvore.java
@@ -17,3 +17,24 @@ public class Arvore {
     }
 
 }
+
+public void in_Ordem(No raiz){
+    if(raiz!=null){
+        in_Ordem(raiz.esquerda);
+        System.out.println(raiz.info);
+        in_Ordem(raiz.direita);
+    }
+
+    public void pre_Ordem(No raiz){
+        if(raiz!=null){
+            System.out.println(raiz.info);
+            in_Ordem(raiz.esquerda);
+            in_Ordem(raiz.direita);
+        }
+
+
+
+}
+
+
+
