9937538ee50157afdde3d29fba6838429e96a581
jntd <jntd@gft.com>
2022-05-15 20:57:13 -0300

    "Node"

--- ArvoreBinaria/Node.java
+++ ArvoreBinaria/Node.java
@@ -1,4 +1,19 @@
 package ArvoreBinaria;
 
 public class Node {
+
+    int info;
+    Node left;
+    Node right;
+
+
+    Node(int info) {
+        this.info=info;
+        this.left=null;
+        this.right=null;
+
+    }
+
+
+
 }
