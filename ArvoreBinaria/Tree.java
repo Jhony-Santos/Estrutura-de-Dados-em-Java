4273a963d632f641e1eb7376830be282aba477be
jntd <jntd@gft.com>
2022-05-16 11:06:51 -0300

    "Métodos PRE-ORDER, IN-ORDER E POS-ORDER adicionados"

--- ArvoreBinaria/Tree.java
+++ ArvoreBinaria/Tree.java
@@ -1,27 +1,33 @@
 package ArvoreBinaria;
 
+import java.sql.SQLOutput;
+
 public class Tree {
 
     Node root = null;
     int size=0;
 
 
-    void insert(int info, Node place){
-        // inserindo pela primeira vez
+    void insert(int info, Node place){ // inserindo pela primeira vez
+
         if (place == null) {
+
             root = new Node(info);
-        }else if(info < place.info){
-            // devo inserir na esquerda
-            if(place.left == null){
+
+        } else if (info < place.info){ // devo inserir na esquerda
+
+            if (place.left == null){
+                System.out.print(" " + info);
                 place.left=new Node(info);
-            }else{
+            } else {
                 insert(info,place.left);
             }
-        }else if(info > place.info){
-            //devo inserir na direita
+
+        } else if (info > place.info){ //devo inserir na direita
             if(place.right== null){
+                System.out.print(" " + info);
                 place.right=new Node(info);
-            }else{
+            } else {
                 insert(info, place.right);
             }
         }
@@ -29,14 +35,36 @@ public class Tree {
 
 
     void preOrder(Node place) {
-        System.out.println(" " + place.info);
-        if(place.left!=null){
+        System.out.print(" " + place.info);
+        if(place.left != null){
             preOrder(place.left);
         }
-        if(place.right!=null){
+        if(place.right != null){
             preOrder(place.right);
         }
     }
+
+    void inOrder(Node place){
+        if(place.left!=null){
+            inOrder(place.left);
+        }
+        System.out.print(" " + place.info );
+        if(place.right != null){
+            inOrder(place.right);
+        }
+    }
+
+    void posOrder(Node place){
+        if(place.left != null){
+            posOrder(place.left);
+        }
+        if(place.right != null){
+            posOrder(place.right);
+        }
+        System.out.print(" " + place.info);
+    }
+
+
 
 
 
